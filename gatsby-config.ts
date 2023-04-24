5323c9ebd6edb880c54aa6bc8795a5879796b103
Michael Hampson <michael_hampson@harvard.edu>
2023-04-23 22:19:58 -0400

    TypeScript

--- gatsby-config.ts
+++ gatsby-config.ts
@@ -1,17 +1,18 @@
-import('gatsby').GatsbyConfig;
+import type { GatsbyConfig } from 'gatsby';
 import path from 'path';
 import { fileURLToPath } from 'url';
 
 // from https://alvin.codes/snippets/migrate-gatsby-esm
 const dirname = path.dirname(fileURLToPath(import.meta.url));
 
-const config = {
+const config: GatsbyConfig = {
   siteMetadata: {
     title: `assignment-5-gatsby`,
     description: `Starter Gatsby project for Assignment 5`,
     course: `CSCI E-114`,
     siteUrl: `https://www.yourdomain.tld`,
   },
+  graphqlTypegen: true,
   plugins: [
     `gatsby-plugin-image`,
     `gatsby-plugin-sharp`,
@@ -21,7 +22,7 @@ const config = {
       resolve: `gatsby-source-filesystem`,
       options: {
         name: `data`,
-        path: path.join(dirname, 'data'),
+        path: path.join(__dirname, 'data'),
       },
     }
   ],
